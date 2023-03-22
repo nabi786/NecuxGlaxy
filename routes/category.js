c978bac17705df67b4af0eb2d97b77519c0df2ec
nabi786 <rehanpardesi2018@gmail.com>
2023-03-22 17:07:12 +0500

    code\

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminControl = require("../controller/adminControl/categoryControl");
 const auth = require("../middleware/auth");
+44;
 // admin Register
 router.post("/create", auth, adminControl.createCategory);
 
